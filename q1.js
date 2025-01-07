fc5058375b4bf34acf72fce044340c4cbbc1d1b9
ernestkhoryh <92951491+ernestkhoryh@users.noreply.github.com>
2025-01-08 03:27:26 +0800

    Create q1.js

---
+++ q1.js
@@ -0,0 +1,9 @@
+function swapValues(x, y) {
+    // Swap values using arithmetic operations
+    newy = x;
+    newx = y;
+    return [newx, newy]; // Return the swapped values as an array
+}
+
+let result = swapValues(5, 10);
+console.log("swapped values of x and y are " + result + " respectively");
